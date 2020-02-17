7cf5bcc78675bc08d7c175b61a36d9a2275f0234
Ramesh <st3vegaming@gmail.com>
2020-02-16 22:04:47 -0500

    Adds a shooting script and implements a shooting mechanics using ray-cast also adds a gun model

---
+++ EZFPS/Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera mainCam;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    public void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+        }
+        
+    }
+}
